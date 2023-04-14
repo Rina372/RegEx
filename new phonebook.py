2fc92e18f78c893da4425837224dfe2f73bef6ea
Rina372 <114817013+Rina372@users.noreply.github.com>
2023-04-14 20:47:36 +0300

    Update new phonebook.py

--- new phonebook.py
+++ new phonebook.py
@@ -1,54 +1,61 @@
 from pprint import pprint
 import re
 import csv
-from collections import defaultdict
+
+data_file = 'phonebook_raw.csv'
+phone_first = r'(\+7|8|7)?[\s*]?\(?(\d{3})\)?[\s*|-]?(\d{3})' \
+               r'[\s*|-]?(\d{2})[\s*|-]?(\d{2})[\s*|-]?\(?(доб\.)' \
+               r'?[\s*|-]?(\d*)?\)?'
+new_phone = r'+7($2)$3-$4-$5 $6$7'
 
 
-with open("phonebook_raw.csv") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-
-    phone_number = r'(\+7|8|7)?[\s*|-]?\(?(\d{3})\)?[\s*|-]?(\d{3})[\s*|-]?(\d{2})[\s*|-]?(\d{2})[\s*|-]?\(?((доб)\.?)\s*(\d+)\)?'
-    correct_phone = r'+7(\2)\3-\4-\5\6\7'
-
-    list_correct = []
-    for page in contacts_list:
-        page_string = ','.join(page)
-        page_format = re.sub(phone_number, phone_number, page_string)
-        page_list = page_format.split(',')
-        list_correct.append(page_list)
+def input_data():
+    with open(data_file, encoding='utf-8') as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+    return contacts_list
 
 
-    pattern_name = r'(^\w[А-ё]+)[\s+|,]?(\w[А-ё]+)[\s+|,]?(\w[А-ё]+)?'
-    new_name = r'\1 \2 \3'
+def contact_list_correct (contacts_list):
+    new_contacts_list = list()
+    for contact in contacts_list:
+        new_contact = list()
+        full_name_str = ','.join(contact[:3])
+        result = re.findall(r'(\w+)', full_name_str)
+        while len(result) < 3:
+            result.append('')
+        new_contact += result
+        new_contact.append(contact[3])
+        new_contact.append(contact[4])
+        phone_pattern = re.compile(phone_first)
+        changed_phone = phone_pattern.sub(new_phone, contact[5])
+        new_contact.append(changed_phone)
+        new_contact.append(contact[6])
+        new_contacts_list.append(new_contact)
+    return new_contacts_list
 
-    contacts_list = []
-    for page in list_correct:
-        page_string = ','.join(page)
-        page_format = re.sub(pattern_name, new_name, page_string)
-        page_list = page_format.split(',')
-        if page_list not in contacts_list:
-            contacts_list.append(page_list)
 
-no_duplicates = defaultdict(list)
-for data in list_correct:
-    key = tuple(data[:2])
-    for item in data:
-        if item not in no_duplicates[key]:
-            no_duplicates[key].append(item)
+def delete_duplicates(new_contacts_list):
+    phone_book = dict()
+    for contact in new_contacts_list:
+        if contact[0] in phone_book:
+            contact_value = phone_book[contact[0]]
+            for i in range(len(contact_value)):
+                if contact[i]:
+                    contact_value[i] = contact[i]
+        else:
+            phone_book[contact[0]] = contact
+    return list(phone_book.values())
 
-correct_result = list(no_duplicates.values())
-pprint(correct_result)
 
-for contact in correct_result:
-    if len(contact) == 7:
-        if '@' not in contact[6] and 'email' not in contact[6]:
-            contact.insert(4, contact[6])
-            del contact[5]
-            contact[6] = ''
-            print(contact)
+def write_data(new_contacts_list):
+    with open('phonebook.csv', 'w', newline='', encoding='utf-8') as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(new_contacts_list)
 
-with open("phonebook.csv", "w") as f:
-    datawriter = csv.writer(f, delimiter=',')
-    datawriter.writerows(correct_result)
 
+new_contacts_list = input_data()
+
+new_list = contact_list_correct(new_contacts_list)
+
+correct_contact_book = delete_duplicates(new_list)
